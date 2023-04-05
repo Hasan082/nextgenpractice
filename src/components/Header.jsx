af4aff31b2f980190be3bfdb2d56c477bfdb1d3e
Hasan <dr.has82@gmail.com>
2023-04-06 01:51:52 +0600

    logo bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <nav className="navbar container">
                 <div className='header-inner'>
                     <div className="logo-wrapper">
-                        <Link to="/"><img src="/src/assets/logo.png" alt="Next gen" className='logo' /></Link>
+                        <Link to="/"><img src="src/assets/logo.png" alt="Next gen" className='logo' /></Link>
                     </div>
                     <div className='nav-inner'>
                         <NavLink to="/">Home</NavLink>
