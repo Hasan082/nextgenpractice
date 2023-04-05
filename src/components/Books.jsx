f31ce691cb6004ce764e9b41209437208780c2f7
Hasan <dr.has82@gmail.com>
2023-04-06 02:48:43 +0600

    Books main page done

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Books = () => {
+    const { books } = useLoaderData()
+    // console.log(books);
     return (
         <div>
-            this is shop page
+            <div className='section'>
+                <div className="container">
+                    <div className="grid grid-colum-4 grid-gap-32">
+                        {
+                            books.map(book => <Link to={book.url} className='single-book'>
+                                <div className="bookimg-wrap">
+                                    <img src={book.image} alt="book" />
+                                </div>
+                                <div className='book-overlay-content'>
+                                    <h4>{book.title}</h4>
+                                    <p>{book.subtitle.substr(0, 40)}...</p>
+                                    <p>Price: {book.price}</p>
+                                </div>
+                            </Link>)
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
